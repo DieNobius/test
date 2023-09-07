b5e4990219a3a76f591cf860dceebd87465ddc3d
Nobius <random@random.random>
2023-09-07 04:03:20 +0300

    Условия

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
 "use strict";
 
 /////////////////////////////////////////
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оценити его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оценити его?', '');
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оценити его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оценити его?', '');
 
 
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+// console.log(personalMovieDB);
+
+
+
+
+// if (1) {
+//   console.log('Ок!');
+// } else {
+//   console.log('Error!');
+// }
+
+
+
+// if (num < 49) {
+//   console.log('Error!');
+// } else if (num > 100) {
+//   console.log('Много');
+// } else {
+//   console.log('Ok');
+// }
+
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+
+// const num = '50';
+
+// switch (num) {
+//   case '49':
+//     console.log('Неверно');
+//     break;
+//   case '100':
+//     console.log('Неверно');
+//     break;
+//   case '50 ':
+//     console.log('Верно');
+//     break;
+//   default:
+//     console.log('Не в этот раз');
+//     break;
+// }
