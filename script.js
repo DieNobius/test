cb198971158ce4d4866e13916a80508a9055af3a
Nobius <random@random.random>
2023-09-14 12:33:05 +0300

    Про return (21)

--- script.js
+++ script.js
@@ -376,13 +376,41 @@
 
 
 
-const usdCurr = 28;
-const eurCurr = 32;
+// const usdCurr = 28;
+// const eurCurr = 32;
 
-function convert(amount, curr) {
-  console.log(curr * amount);
+// function convert(amount, curr) {
+//   console.log(curr * amount);
 
-}
+// }
 
-convert(500, usdCurr);
-convert(500, eurCurr);
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//  return curr * amount;
+
+// }
+
+// function promotion(result) {
+//   console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr)
+// promotion(res);
+
+
+
+// function test() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     if (i === 6) return
+//   }
+//   console.log('Done');
+// }
+
+// test();
