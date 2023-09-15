ac928be0594de07bccce2480e747b84089c7466a
Nobius <random@random.random>
2023-09-15 11:10:20 +0300

    Методы и свойства строк и чисел (22)

--- script.js
+++ script.js
@@ -414,3 +414,35 @@
 // }
 
 // test();
+
+
+
+
+// const str = 'teSt';
+
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+
+// const fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('q'));
+
+
+// console.log(logg.slice(-5, -1));
+// console.log(logg.slice(-5, -1));
+
+// console.log(logg.substring(6, 11));
+
+
+// console.log(logg.substr(6, 5));
+// const logg = 'Hello world';
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = '12.2px'
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
