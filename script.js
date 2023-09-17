92664810dbe48978b79e76b254b7d2a55e405cfe
Nobius <random@random.random>
2023-09-17 11:38:29 +0300

    Callback функции (25)

--- script.js
+++ script.js
@@ -447,3 +447,28 @@
 
 
 
+function first() {
+  //
+  setTimeout(function () {
+    console.log(1);
+  }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+  console.log(`я учу ${lang}`);
+  callback();
+}
+
+
+function done() {
+  console.log('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done);
