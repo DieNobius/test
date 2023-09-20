bf35e8119f62aae80a249495d909f5906083267e
Nobius <random@random.random>
2023-09-20 12:24:13 +0300

    Передача по ссылке или по значению, Spread оператор (29)

--- script.js
+++ script.js
@@ -64,12 +64,12 @@
 // }
 
 
-  // const hamburger = 5;
-  // const fries = null; 
+// const hamburger = 5;
+// const fries = null; 
 
-  // if (hamburger && fries) {
-  //   console.log('Я сыт');
-  // }
+// if (hamburger && fries) {
+//   console.log('Я сыт');
+// }
 
 // console.log((hamburger && fries));
 
@@ -119,7 +119,7 @@
 
 
 // console.log(!0);
- 
+
 
 
 
@@ -247,7 +247,7 @@
 // const length = 7;
 
 // for (let i = 1; i < length; i++) {
-   
+
 //   for (let j = 0; j < i; j++) {
 //     result += '*';
 //   }
@@ -282,7 +282,7 @@
 
 
 
- 
+
 // const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '')
 
 // const 
@@ -509,9 +509,9 @@
 // console.log(counter);
 
 // console.log(options.name);
- 
+
 // delete options.name;
-  
+
 // console.log(options);
 
 
@@ -555,3 +555,104 @@
 // products.sort();
 // console.log(products.join('; '));
 
+
+
+// let a = 5,
+//     b = a;
+
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+
+// function copy (mainObj) {
+//     let objCopy = {};
+
+//     let  key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+// console.log(newNumbers);
+// console.log(numbers);
+
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+
+
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+
+// newArray[1] = '123213213';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livehournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+
+// log(...num)
+
+
+// const array = ['a', 'b'];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newOnbj = {...q};
