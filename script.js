b40a7969143eb175efa958dc7bb4733846892dfe
Nobius <random@random.random>
2023-09-21 09:58:24 +0300

    Основы ООП (30)

--- script.js
+++ script.js
@@ -657,3 +657,35 @@
 
 // const newOnbj = {...q};
 
+
+
+// let str = 'some';
+// let strObj = new  String(str);
+
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function () {
+//         console.log('Hello');
+//     }
+// };
+
+// const john = Object.create(soldier);
+
+// const john = {
+//     health: 100
+// };
+
+// john.__proto__ = solder;
+
+
+// Object.setPrototypeOf(john, solder);
+
+// console.log(john.armor);
+// john.sayHello();
