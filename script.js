aa1845f809b44f2b9fbc4992ba2dc60634cb2493
Nobius <random@random.random>
2023-09-25 10:24:24 +0300

    Динамическая типизация в JSе (33)

--- script.js
+++ script.js
@@ -690,3 +690,63 @@
 // console.log(john.armor);
 // john.sayHello();
 
+
+ // To String
+
+ // 1)
+
+//  console.log(typeof(String(null)));
+//  console.log(typeof(String(4)));
+
+ // 2)
+// console.log(typeof(null + ''));
+
+// const num = 5;
+
+// console.log('https://google.com/catalog/' + num);
+
+// const fontSize = 26 + 'px';
+
+// To Number 
+// 1)
+
+// console.log(typeof(Number('4')));
+
+//2)
+
+// console.log(typeof((5 + '4')));
+// console.log(typeof((5 + +'4')));
+
+//3)
+
+// console.log(typeof(parseInt('15px', 10)));
+
+
+// let answ = +prompt('Hello', '');
+
+// To boolean
+
+// 0, '', null, undefined, NaN; = false
+
+// 1)
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working');
+// }
+
+// 2)
+
+//console.log(typeof(Boolean('4')));
+
+// 3)
+
+
+// console.log(typeof(!!'434'));
