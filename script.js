fbb69044c22dfa7bea9c11541bc910093fdf3045
Nobius <random@random.random>
2023-09-26 13:03:24 +0300

    Замыкание и лексическое окружение(34)

--- script.js
+++ script.js
@@ -691,14 +691,14 @@
 // john.sayHello();
 
 
- // To String
+// To String
 
- // 1)
+// 1)
 
 //  console.log(typeof(String(null)));
 //  console.log(typeof(String(4)));
 
- // 2)
+// 2)
 // console.log(typeof(null + ''));
 
 // const num = 5;
@@ -748,5 +748,42 @@
 
 // 3)
 
-
 // console.log(typeof(!!'434'));
+
+
+// let number = 5; debugger
+
+// function logNumber() {
+//     // let number = 4; debugger
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber(); debugger
+
+// number = 8;
+
+// logNumber(); debugger
+
+
+// function createCounter() {
+//     let counter = 0;
+
+//     const myFunction = function () {
+//         debugger
+//         counter = counter + 1; debugger
+//         return counter; debugger
+//     }
+
+//     return myFunction;
+// }
+// debugger
+// const increment = createCounter();
+// const c1 = increment(); debugger
+// const c2 = increment(); debugger
+// const c3 = increment(); debugger
+
+// console.log(c1, c2, c3);
+
+
